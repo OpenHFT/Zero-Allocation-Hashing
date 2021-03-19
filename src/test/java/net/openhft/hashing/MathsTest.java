e322e0fb94f1d5eba96ff14563ff77a7840e3fc0
James Z.M. Gao <gaozm55@gmail.com>
2021-03-19 18:54:19 +0800

    Add xxh128

--- src/test/java/net/openhft/hashing/MathsTest.java
+++ src/test/java/net/openhft/hashing/MathsTest.java
@@ -11,16 +11,19 @@ public class MathsTest {
             long x = 0x100000001L;
             long y = 0x200000002L;
             assertEquals(2L ^ 0x400000002L, Maths.unsignedLongMulXorFold(x, y));
+            assertEquals(2L, Maths.unsignedLongMulHigh(x, y));
         }
         {
             long x = -1;
             long y = -1;
             assertEquals((-2) ^ 1, Maths.unsignedLongMulXorFold(x, y));
+            assertEquals(-2L, Maths.unsignedLongMulHigh(x, y));
         }
         {
             long x = -1;
             long y = 0x300000003L;
             assertEquals(0x300000002L ^ (-0x300000003L), Maths.unsignedLongMulXorFold(x, y));
+            assertEquals(0x300000002L, Maths.unsignedLongMulHigh(x, y));
         }
     }
 }
