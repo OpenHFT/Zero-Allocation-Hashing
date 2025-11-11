a0e6afb866789782a970b031e90b7dec276c8d1b
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 16:18:21 +0000

    restore the javadoc

--- src/main/java/net/openhft/hashing/package-info.java
+++ src/main/java/net/openhft/hashing/package-info.java
@@ -1,8 +1,6 @@
 /*
  * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
-
-
 /**
  * API for hashing sequential data and zero-allocation, pretty fast implementations
  * of non-cryptographic hash functions.
